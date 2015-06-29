d79ae244a964869c99e0ead30dee32507731f099
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-06-29 11:34:54 -0400

    test log in
    not finished

---
+++ src/TestLogIn.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestLogIn {
+	public static void main(String[] args){
+		String email = "lxtlouise@163.com";
+		String password = "";
+		WebDriver driver = new FirefoxDriver();
+		  
+		driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_signin");
+	    
+		WebElement emailAddress = driver.findElement(By.id("ap_email"));
+		emailAddress.sendKeys(email);
+		
+		WebElement pass = driver.findElement(By.id("ap_password"));
+		pass.sendKeys(password);
+		
+		emailAddress.submit();
+		pass.submit();
+		
+	}
+  
+}
