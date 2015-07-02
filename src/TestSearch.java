c82e47d1b1c411e956fa22a332248077f8723cc8
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-07-01 20:43:22 -0400

    change to test
    change to test

--- src/TestSearch.java
+++ src/TestSearch.java
@@ -1,17 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class TestSearch {
-  public static void main(String[] args){
-	  WebDriver driver = new FirefoxDriver();
-	  driver.get("www.amazon.com");
+
+	  static WebDriver driver = new HtmlUnitDriver();
 	  
-	  WebElement search = driver.findElement(By.id("twotabssearchtextbox"));
-	  search.sendKeys("Software");
-	  search.submit();
+	  @Before
+		public void setUp() throws Exception {
+		  driver.get("http://www.amazon.com");
+		}
 	  
+	  @Test
+	    public void testSearchItems(){
+	      WebElement search = driver.findElement(By.id("twotabsearchtextbox"));
+	      search.sendKeys("Software");
+	      search.submit();
+	  }  
 	  
-  }
+	 
+  
 }
