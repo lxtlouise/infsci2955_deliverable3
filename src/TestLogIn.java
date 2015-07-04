2eb72e1f479e0067ce17afc07099b774c2a21e6e
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-07-04 15:25:29 -0400

    log in finish
    have to fill in the email and password to make it work

--- src/TestLogIn.java
+++ src/TestLogIn.java
@@ -1,26 +1,69 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class TestLogIn {
-	public static void main(String[] args){
-		String email = "lxtlouise@163.com";
-		String password = "";
-		WebDriver driver = new FirefoxDriver();
-		  
+	
+	//Test log in.
+	//Enter invalid username and invalid password, the user will be shown with a warning message.
+	@Test
+	public void testLogInvalid(){
+		WebDriver driver = new HtmlUnitDriver();
 		driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_signin");
-	    
-		WebElement emailAddress = driver.findElement(By.id("ap_email"));
-		emailAddress.sendKeys(email);
+
+		String email = "1234";
+		String password = "1234";
 		
-		WebElement pass = driver.findElement(By.id("ap_password"));
-		pass.sendKeys(password);
+		WebElement em = driver.findElement(By.id("ap_email"));
+		em.sendKeys(email);
+		em.submit();
+		WebElement pa = driver.findElement(By.id("ap_password"));
+		pa.sendKeys(password);
+		pa.submit();
 		
-		emailAddress.submit();
-		pass.submit();
+		try {
+			WebElement result = driver.findElement(By.id("message_warning")).findElement(By.tagName("h6"));
+			String actual = result.getText();
+			String expected = "Important Message!";
+			assertEquals(expected, actual);
+			driver.quit();
+		} catch (NoSuchElementException e) {
+			fail();
+		}
 		
+	}
+	
+	//Test log in.
+	//Enter valid username and valid password, the user will be able to log in to the account and start to do the shopping.
+	@Test
+	public void testLogValid(){
+		WebDriver driver = new HtmlUnitDriver();
+		driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_signin");
+
+		String email = "";
+		String password = "";
+		
+		WebElement em = driver.findElement(By.id("ap_email"));
+		em.sendKeys(email);
+		em.submit();
+		WebElement pa = driver.findElement(By.id("ap_password"));
+		pa.sendKeys(password);
+		pa.submit();
+		
+		try {
+			WebElement result = driver.findElement(By.id("nav-your-amazon"));
+			String actual = result.getText();
+			String expected = "Louise's Amazon.com";
+			assertEquals(expected, actual);
+			driver.quit();
+		} catch (NoSuchElementException e) {
+			fail();
+		}
 	}
   
 }
