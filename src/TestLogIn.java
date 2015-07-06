c6753347ab2df29669664e1013eb6c8af2f3b71a
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-07-06 16:01:43 -0400

    add email and password
    add email and password

--- src/TestLogIn.java
+++ src/TestLogIn.java
@@ -45,8 +45,8 @@ public class TestLogIn {
 		WebDriver driver = new HtmlUnitDriver();
 		driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_signin");
 
-		String email = "";
-		String password = "";
+		String email = "2014shufehey@gmail.com";
+		String password = "2014shufe";
 		
 		WebElement em = driver.findElement(By.id("ap_email"));
 		em.sendKeys(email);
@@ -58,7 +58,7 @@ public class TestLogIn {
 		try {
 			WebElement result = driver.findElement(By.id("nav-your-amazon"));
 			String actual = result.getText();
-			String expected = "Louise's Amazon.com";
+			String expected = "2014shu...'s Amazon.com";
 			assertEquals(expected, actual);
 			driver.quit();
 		} catch (NoSuchElementException e) {
